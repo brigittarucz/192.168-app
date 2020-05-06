8e516b6331c7364ddd5abba6ecd03c281df809bd
Brigitta <brigitta.rucz@yahoo.com>
2020-05-06 15:50:02 +0200

    age check + auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSelectModule } from '@angular/material/select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgeCheckComponent } from './components/views/age-check/age-check.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationComponent } from './components/views/authentication/authentication.component';
+import { LoginComponent } from './components/views/authentication/login/login.component';
+import { SignupComponent } from './components/views/authentication/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, AgeCheckComponent, AuthenticationComponent, LoginComponent, SignupComponent ],
+	imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSelectModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/views/age-check/age-check.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-age-check',
+	templateUrl: './age-check.component.html',
+	styleUrls: [ './age-check.component.scss' ]
+})
+export class AgeCheckComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
