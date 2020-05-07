8441050c9db2f0b7fd6e99d84d52919f05fe04f3
Brigitta <brigitta.rucz@yahoo.com>
2020-05-07 14:42:12 +0200

    style checks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TourComponent } from './components/views/tour/tour.component';
 import { DashboardComponent } from './components/views/dashboard/dashboard.component';
+import { AuthenticationComponent } from './components/views/authentication/authentication.component';
 
 const routes: Routes = [
+	{ path: '', component: AuthenticationComponent },
 	{ path: 'intro-tour', component: TourComponent },
 	{ path: 'dashboard', component: DashboardComponent }
 ];
--- src/app/components/views/dashboard/dashboard.component.ts
+++ src/app/components/views/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './dashboard.component.scss' ]
 })
 export class DashboardComponent implements OnInit {
+	skipTour: boolean = true;
+
 	constructor() {}
 
 	ngOnInit(): void {}
