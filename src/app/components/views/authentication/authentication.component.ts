f4c0bc55b227999397a49ba62149dd7f0c68fc0b
Brigitta <brigitta.rucz@yahoo.com>
2020-05-07 10:32:00 +0200

    login signup toggle

--- src/app/components/views/authentication/authentication.component.ts
+++ src/app/components/views/authentication/authentication.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './authentication.component.scss' ]
 })
 export class AuthenticationComponent implements OnInit {
+	isLoginActive: boolean = true;
+	isSignupActive: boolean = false;
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	displayLoginScreen() {
+		this.isLoginActive = true;
+		this.isSignupActive = false;
+	}
+
+	displaySignupScreen() {
+		this.isLoginActive = false;
+		this.isSignupActive = true;
+	}
 }
